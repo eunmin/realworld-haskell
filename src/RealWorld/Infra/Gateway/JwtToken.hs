ae2044f1304b95790fbd42f30e400e9b243d245b
Eunmin Kim <telepopsound@gmail.com>
2024-02-15 16:27:47 +0900

    토큰 만료 확인

--- src/RealWorld/Infra/Gateway/JwtToken.hs
+++ src/RealWorld/Infra/Gateway/JwtToken.hs
@@ -6,15 +6,20 @@ import qualified Data.Aeson.Types as JSON
 import Data.Has
 import Data.Map.Strict (lookup)
 import Data.Text (unpack)
+import Data.Time
+import Data.Time.Clock.POSIX (getPOSIXTime)
 import Data.ULID (ULID)
 import RealWorld.Domain.User.Entity (Token (..))
-import Relude hiding (State)
+import Relude hiding (State, exp)
 import Web.JWT
   ( ClaimsMap (..),
-    JWTClaimsSet (unregisteredClaims),
+    JWT,
+    JWTClaimsSet (exp, unregisteredClaims),
+    VerifiedJWT,
     decodeAndVerifySignature,
     encodeSigned,
     hmacSecret,
+    numericDate,
     toVerify,
   )
 import qualified Web.JWT as JWT
@@ -25,21 +30,29 @@ signJWT claims secret =
   where
     claimSet = mempty {unregisteredClaims = ClaimsMap claims}
 
-unsignJWT :: Text -> Text -> Maybe (Map Text JSON.Value)
-unsignJWT token secret = do
-  jwt <- decodeAndVerifySignature (toVerify . hmacSecret $ secret) token
-  pure $ unClaimsMap . unregisteredClaims . JWT.claims $ jwt
+unsignJWT :: Text -> Text -> NominalDiffTime -> Maybe (Map Text JSON.Value)
+unsignJWT jwt secret currentTime = do
+  verifiedJwt <- decodeAndVerifySignature (toVerify . hmacSecret $ secret) jwt
+  ifM
+    (isExpired verifiedJwt)
+    Nothing
+    $ pure (unClaimsMap . unregisteredClaims . JWT.claims $ verifiedJwt)
+  where
+    isExpired :: JWT VerifiedJWT -> Maybe Bool
+    isExpired verifiedJWT =
+      (<) <$> (exp . JWT.claims $ verifiedJWT) <*> numericDate currentTime
 
 generate :: (Has Text r, MonadState r m) => ULID -> m Token
 generate userId = do
   secret <- gets getter
   pure $ Token $ signJWT (fromList [("userId", JSON.String $ show userId)]) secret
 
-verify :: (Has Text r, MonadState r m) => Token -> m (Maybe ULID)
+verify :: (Has Text r, MonadState r m, MonadIO m) => Token -> m (Maybe ULID)
 verify (Token token) = do
   secret :: Text <- gets getter
+  now <- liftIO getPOSIXTime
   pure $ do
-    claims <- unsignJWT token secret
+    claims <- unsignJWT token secret now
     userId <- lookup "userId" claims
     valutToText userId >>= readMaybe . unpack
   where
